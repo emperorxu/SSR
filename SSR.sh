61b56dcbdb063c5e34502940912f75f1f925d2b0
emperorxu <emperorxu@sina.com>
2018-10-11 17:17:59 +0800

    CH

--- SSR.sh
+++ SSR.sh
@@ -1,31 +1,26 @@
-#!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#=================================================================#
-#   系统要求:  CentOS 6,7, Debian, Ubuntu              		       #
-#   简介: 一键安装shadowsocksr服务器          					  #
-#=================================================================#
 
 clear
 echo
 echo 
 echo "#############################################################"
-echo "# 一键安装shadowsocksr服务器                                #"
-echo "# 系统要求:  CentOS 6,7, Debian, Ubuntu                     #"
+echo "一键安装shadowsocksr服务器"
+echo "系统要求:  CentOS 6,7, Debian, Ubuntu"
 echo "#############################################################"
 
-#Current folder
+#获取当前路径
 cur_dir=`pwd`
 
-# Make sure only root can run our script
+#确保ROOT权限
 rootness(){
     if [[ $EUID -ne 0 ]]; then
-       echo "Error: This script must be run as root!" 1>&2
+       echo "Error: 此脚本必须以root身份运行！" 1>&2
        exit 1
     fi
 }
 
-# Disable selinux
+#禁用SElinux
 disable_selinux(){
     if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
         sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
@@ -33,7 +28,7 @@ disable_selinux(){
     fi
 }
 
-#Check system
+#判定操作系统
 check_sys(){
     local checkType=$1
     local value=$2
@@ -79,7 +74,7 @@ check_sys(){
     fi
 }
 
-# Get version
+#获取版本
 getversion(){
     if [[ -s /etc/redhat-release ]]; then
         grep -oE  "[0-9.]+" /etc/redhat-release
@@ -88,7 +83,7 @@ getversion(){
     fi
 }
 
-# CentOS version
+#CentOS 版本
 centosversion(){
     if check_sys sysRelease centos; then
         local code=$1
@@ -104,7 +99,7 @@ centosversion(){
     fi
 }
 
-# Get public IP address
+#获取IP地址
 get_ip(){
     local IP=$( ip addr | egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | egrep -v "^192\.168|^172\.1[6-9]\.|^172\.2[0-9]\.|^172\.3[0-2]\.|^10\.|^127\.|^255\.|^0\." | head -n 1 )
     [ -z ${IP} ] && IP=$( wget -qO- -t1 -T2 ipv4.icanhazip.com )
@@ -112,19 +107,19 @@ get_ip(){
     [ ! -z ${IP} ] && echo ${IP} || echo
 }
 
-# Pre-installation settings
+#预安装设置
 pre_install(){
     if check_sys packageManager yum || check_sys packageManager apt; then
-        # Not support CentOS 5
+        #不支持CentOS 5
         if centosversion 5; then
-            echo "Error: Not supported CentOS 5, please change to CentOS 6+/Debian 7+/Ubuntu 12+ and try again."
+            echo "Error:不支持CentOS 5,请在CentOS 6+/Debian 7+/Ubuntu 12+系统中再次尝试安装。"
             exit 1
         fi
     else
-        echo "Error: Your OS is not supported. please change OS to CentOS/Debian/Ubuntu and try again."
+        echo "Error:暂不支持您的操作系统。请在CentOS 6+/Debian 7+/Ubuntu 12+系统中再次尝试安装。"
         exit 1
     fi
-    # Set ShadowsocksR config password
+    #设置ShadowsocksR密码
     echo "请输入要设置SSR的密码:"
     read -p "(密码默认: q123123):" shadowsockspwd
     [ -z "${shadowsockspwd}" ] && shadowsockspwd="q123123"
@@ -133,7 +128,7 @@ pre_install(){
     echo "password = ${shadowsockspwd}"
     echo "---------------------------"
     echo
-    # Set ShadowsocksR config port
+    #设置ShadowsocksR端口
     while true
     do
     echo -e "请输入ssr端口 [1-65535]:"
@@ -149,10 +144,10 @@ pre_install(){
             echo
             break
         else
-            echo "Input error, please input correct number"
+            echo "输入错误，请输入正确的数字"
         fi
     else
-        echo "Input error, please input correct number"
+        echo "输入错误，请输入正确的数字"
     fi
     done
     get_char(){
@@ -167,7 +162,7 @@ pre_install(){
     echo
     echo "按任意键开始安装…或按Ctrl + C取消安装"
     char=`get_char`
-    # Install necessary dependencies
+    #安装必要的依赖项
     if check_sys packageManager yum; then
         yum install -y unzip openssl-devel gcc swig python python-devel python-setuptools autoconf libtool libevent automake make curl curl-devel zlib-devel perl perl-devel cpio expat-devel gettext-devel
     elif check_sys packageManager apt; then
@@ -177,35 +172,35 @@ pre_install(){
     cd ${cur_dir}
 }
 
-# Download files
+#下载文件
 download_files(){
-    # Download libsodium file
+    #下载libsodium
     if ! wget --no-check-certificate -O libsodium-1.0.11.tar.gz https://github.com/emperorxu/SSR/libsodium-1.0.11.tar.gz; then
-        echo "Failed to download libsodium-1.0.11.tar.gz!"
+        echo "libsodium-1.0.11.tar.gz下载失败!"
         exit 1
     fi
-    # Download ShadowsocksR file
+    #下载ShadowsocksR
     if ! wget --no-check-certificate -O manyuser.zip https://github.com/emperorxu/SSR/shadowsocks-manyuser.zip; then
-        echo "Failed to download ShadowsocksR file!"
+        echo "ShadowsocksR下载失败!"
         exit 1
     fi
-    # Download ShadowsocksR init script
+    #下载ShadowsocksR init脚本
     if check_sys packageManager yum; then
         if ! wget --no-check-certificate https://github.com/emperorxu/SSR/shadowsocksr -O /etc/init.d/shadowsocks; then
-            echo "Failed to download ShadowsocksR chkconfig file!"
+            echo "ShadowsocksR chkconfig下载失败!"
             exit 1
         fi
     elif check_sys packageManager apt; then
         if ! wget --no-check-certificate https://github.com/emperorxu/SSR/shadowsocksr/debian -O /etc/init.d/shadowsocks; then
-            echo "Failed to download ShadowsocksR chkconfig file!"
+            echo "ShadowsocksR chkconfig下载失败!"
             exit 1
         fi
     fi
 }
 
-# Firewall set
+#防火墙配置
 firewall_set(){
-    echo "firewall set start..."
+    echo "开始配置防火墙..."
     if centosversion 6; then
         /etc/init.d/iptables status > /dev/null 2>&1
         if [ $? -eq 0 ]; then
@@ -216,10 +211,10 @@ firewall_set(){
                 /etc/init.d/iptables save
                 /etc/init.d/iptables restart
             else
-                echo "port ${shadowsocksport} has been set up."
+                echo "端口 ${shadowsocksport} 已配置."
             fi
         else
-            echo "WARNING: iptables looks like shutdown or not installed, please manually set it if necessary."
+            echo "WARNING: iptables 已关闭或未安装, 如需请手动配置."
         fi
     elif centosversion 7; then
         systemctl status firewalld > /dev/null 2>&1
@@ -228,21 +223,21 @@ firewall_set(){
             firewall-cmd --permanent --zone=public --add-port=${shadowsocksport}/udp
             firewall-cmd --reload
         else
-            echo "Firewalld looks like not running, try to start..."
+            echo "Firewalld 似乎未在运行, 尝试启动..."
             systemctl start firewalld
             if [ $? -eq 0 ]; then
                 firewall-cmd --permanent --zone=public --add-port=${shadowsocksport}/tcp
                 firewall-cmd --permanent --zone=public --add-port=${shadowsocksport}/udp
                 firewall-cmd --reload
             else
-                echo "WARNING: Try to start firewalld failed. please enable port ${shadowsocksport} manually if necessary."
+                echo "WARNING: firewalld 启动失败. 如果需要，请手动启用端口 ${shadowsocksport} ."
             fi
         fi
     fi
-    echo "firewall set completed..."
+    echo "防火墙配置完成..."
 }
 
-# Config ShadowsocksR
+#配置ShadowsocksR
 config_shadowsocks(){
     cat > /etc/shadowsocks.json<<-EOF
 {
@@ -255,9 +250,9 @@ config_shadowsocks(){
     "password":"${shadowsockspwd}",
     "timeout":120,
     "method":"chacha20",
-    "protocol":"auth_sha1_v4_compatible",
+    "protocol":"origin",
     "protocol_param":"",
-    "obfs":"tls1.2_ticket_auth_compatible",
+    "obfs":"http_simple",
     "obfs_param":"",
     "redirect":"",
     "connect_verbose_info": 1,
@@ -268,20 +263,20 @@ config_shadowsocks(){
 EOF
 }
 
-# Install ShadowsocksR
+#安装ShadowsocksR
 install(){
-    # Install libsodium
+    #安装libsodium
     tar zxf libsodium-1.0.11.tar.gz
     cd libsodium-1.0.11
     ./configure && make && make install
     if [ $? -ne 0 ]; then
-        echo "libsodium install failed!"
+        echo "libsodium安装失败"
         install_cleanup
         exit 1
     fi
     echo "/usr/local/lib" > /etc/ld.so.conf.d/local.conf
     ldconfig
-    # Install ShadowsocksR
+    #安装ShadowsocksR
     cd ${cur_dir}
     unzip -q manyuser.zip
     mv shadowsocks-manyuser/shadowsocks /usr/local/
@@ -303,27 +298,27 @@ install(){
         echo -e "Password: \033[41;37m ${shadowsockspwd} \033[0m"
         echo -e "Local IP: \033[41;37m 127.0.0.1 \033[0m"
         echo -e "Local Port: \033[41;37m 1080 \033[0m"
-        echo -e "Protocol: \033[41;37m auth_sha1_v4_compatible \033[0m"
-        echo -e "obfs: \033[41;37m tls1.2_ticket_auth_compatible \033[0m"
+        echo -e "Protocol: \033[41;37m origin \033[0m"
+        echo -e "obfs: \033[41;37m http_simple \033[0m"
         echo -e "Encryption Method: \033[41;37m chacha20 \033[0m"
         echo
-        echo "SSR安装成功，尽情享用!"
+        echo "SSR安装成功!"
         echo
     else
-        echo "shadowsocksr安装失败"
+        echo "SSR安装失败!"
         install_cleanup
         exit 1
     fi
 }
 
-# Install cleanup
+#清理安装
 install_cleanup(){
     cd ${cur_dir}
     rm -rf manyuser.zip shadowsocks-manyuser libsodium-1.0.11.tar.gz libsodium-1.0.11
 }
 
 
-# Uninstall ShadowsocksR
+#卸载ShadowsocksR
 uninstall_shadowsocks(){
     printf "确定卸载 ShadowsocksR? (y/n)"
     printf "\n"
@@ -343,15 +338,15 @@ uninstall_shadowsocks(){
         rm -f /etc/init.d/shadowsocks
         rm -f /var/log/shadowsocks.log
         rm -rf /usr/local/shadowsocks
-        echo "ShadowsocksR uninstall success!"
+        echo "ShadowsocksR卸载成功!"
     else
         echo
-        echo "uninstall cancelled, nothing to do..."
+        echo "卸载取消..."
         echo
     fi
 }
 
-# Install ShadowsocksR
+#安装ShadowsocksR
 install_shadowsocks(){
     rootness
     disable_selinux
@@ -365,7 +360,7 @@ install_shadowsocks(){
     install_cleanup
 }
 
-# Initialization step
+#初始化
 action=$1
 [ -z $1 ] && action=install
 case "$action" in
@@ -373,7 +368,7 @@ case "$action" in
     ${action}_shadowsocks
     ;;
     *)
-    echo "Arguments error! [${action}]"
+    echo "变量错误! [${action}]"
     echo "Usage: `basename $0` {install|uninstall}"
     ;;
 esac
